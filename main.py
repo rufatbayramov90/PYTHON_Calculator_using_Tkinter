6b110e02c524dc8c1ab48849a86770b79deae504
rufatbayramov90 <rufatbyrmv@gmail.com>
2024-04-01 11:51:50 +0400

    recent changes

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ class Calculator:
 
         self.equation = StringVar()
         self.entry_value = ''
-        Entry(width=17, bg='#fff', font=('Arial Bold',28),textvariable=self.equation).place(x=0,y=0)
+        Entry(width=17, bg='#ccddff', font=('Arial Bold',28),textvariable=self.equation).place(x=0,y=0)
 
-        Button(width=11, height=4,text='(',relief='flat',bg='white',command=lambda :self.show('(')).place(x=0,y=50)
+        Button(width=11, height=4, text='(', relief='flat', bg='white', command=lambda: self.show('(')).place(x=0,y=50)
         Button(width=11, height=4, text=')', relief='flat', bg='white', command=lambda: self.show(')')).place(x=90, y=50)
-        Button(width=11, height=4, text='%', relief='flat', bg='white', command=lambda: self.show('%')).place(x=100, y=50)
+        Button(width=11, height=4, text='%', relief='flat', bg='white', command=lambda: self.show('%')).place(x=180, y=50)
         Button(width=11, height=4, text='1', relief='flat', bg='white', command=lambda: self.show(1)).place(x=0, y=125)
         Button(width=11, height=4, text='2', relief='flat', bg='white', command=lambda: self.show(2)).place(x=90, y=125)
         Button(width=11, height=4, text='3', relief='flat', bg='white', command=lambda: self.show(3)).place(x=180, y=125)
@@ -25,12 +25,12 @@ class Calculator:
         Button(width=11, height=4, text='9', relief='flat', bg='white', command=lambda: self.show(9)).place(x=90, y=275)
         Button(width=11, height=4, text='0', relief='flat', bg='white', command=lambda: self.show(0)).place(x=90, y=350)
         Button(width=11, height=4, text='.', relief='flat', bg='white', command=lambda: self.show('.')).place(x=180, y=350)
-        Button(width=11, height=4, text='+', relief='flat', bg='white', command=lambda: self.show('+')).place(x=270, y=350)
+        Button(width=11, height=4, text='+', relief='flat', bg='white', command=lambda: self.show('+')).place(x=270, y=275)
         Button(width=11, height=4, text='-', relief='flat', bg='white', command=lambda: self.show('-')).place(x=270, y=200)
         Button(width=11, height=4, text='/', relief='flat', bg='white', command=lambda: self.show('/')).place(x=270, y=50)
-        Button(width=11, height=4, text='x', relief='flat', bg='white', command=lambda: self.show('x')).place(x=270, y=125)
-        Button(width=11, height=4, text='=', relief='flat', bg='white', command=self.solve).place(x=270,y=350)
-        Button(width=11, height=4, text='C', relief='flat', bg='white', command=self.clear).place(x=0,y=350)
+        Button(width=11, height=4, text='*', relief='flat', bg='white', command=lambda: self.show('*')).place(x=270, y=125)
+        Button(width=11, height=4, text='=', relief='flat', bg='lightblue', command=self.solve).place(x=270,y=350)
+        Button(width=11, height=4, text='C', relief='flat', command=self.clear).place(x=0,y=350)
 
 
 
